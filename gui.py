bb8cfc23c4a4032ed5c727dc5bac6b1bcce54d28
Jongwon Hwang <hjw8707@gmail.com>
2022-12-29 10:13:36 +0900

    Commit again (adding all other modifications)

--- gui.py
+++ gui.py
@@ -6,17 +6,17 @@ from nchart import nchart
 
 from PyQt6 import QtCore, QtWidgets
 
+from matplotlib.backends import backend_qtagg
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
 
 class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
         #self.axes = fig.add_subplot(111, frameon = False)
-        self.axes = fig.add_axes((0,0,1,1))
-        self.nc = nchart(self.axes)    
-        super(MplCanvas, self).__init__(fig)
+        self.nc = nchart(self.fig)    
+        super(MplCanvas, self).__init__(self.fig)
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -25,7 +25,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Create the maptlotlib FigureCanvas object,
         # which defines a single set of axes as self.axes.
-        self.sc = MplCanvas(self, width=8, height=8, dpi=100)
+        self.sc = MplCanvas(self, width=5, height=5, dpi=100)
         #sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
         # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
         toolbar = NavigationToolbar(self.sc, self)
@@ -34,6 +34,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.maxZSB = QtWidgets.QSpinBox(); self.maxZSB.setValue(20); self.maxZSB.setRange(0,120)
         self.minNSB = QtWidgets.QSpinBox(); self.minNSB.setValue(0) ; self.minNSB.setRange(0,300)
         self.maxNSB = QtWidgets.QSpinBox(); self.maxNSB.setValue(20); self.maxNSB.setRange(0,300)
+        self.valCB = QtWidgets.QComboBox(); self.valCB.addItems(["Major Decay", "Stability", "Lifetime", "Binding E"]); self.valCB.setCurrentIndex(0)
         self.fontSizeSB = QtWidgets.QSpinBox(); self.fontSizeSB.setValue(5); self.fontSizeSB.setRange(0,60)
         self.borderWidthSB = QtWidgets.QDoubleSpinBox(); self.borderWidthSB.setValue(0.1); self.borderWidthSB.setRange(0,10); self.borderWidthSB.setSingleStep(0.1)
                 
@@ -46,16 +47,21 @@ class MainWindow(QtWidgets.QMainWindow):
         setLayout1.addWidget(self.minNSB)
         setLayout1.addWidget(QtWidgets.QLabel("Max. N"))        
         setLayout1.addWidget(self.maxNSB)
-        
+
         setLayout2 = QtWidgets.QHBoxLayout()
-        setLayout2.addWidget(QtWidgets.QLabel("Font Size"))            
-        setLayout2.addWidget(self.fontSizeSB)
-        setLayout2.addWidget(QtWidgets.QLabel("Border Width"))            
-        setLayout2.addWidget(self.borderWidthSB)
+        setLayout2.addWidget(QtWidgets.QLabel("Coloring"))            
+        setLayout2.addWidget(self.valCB)
+                
+        setLayout3 = QtWidgets.QHBoxLayout()
+        setLayout3.addWidget(QtWidgets.QLabel("Font Size"))            
+        setLayout3.addWidget(self.fontSizeSB)
+        setLayout3.addWidget(QtWidgets.QLabel("Border Width"))            
+        setLayout3.addWidget(self.borderWidthSB)
                 
         setLayout = QtWidgets.QVBoxLayout()
         setLayout.addLayout(setLayout1)
         setLayout.addLayout(setLayout2)
+        setLayout.addLayout(setLayout3)
         
         refButton = QtWidgets.QPushButton("Refresh", self)
         refButton.clicked.connect(self.refresh)
@@ -82,12 +88,14 @@ class MainWindow(QtWidgets.QMainWindow):
         
 
     def refresh(self):
-        self.sc.axes.clear()
+        self.sc.fig.clear()
         
         self.sc.nc.z_min = self.minZSB.value()
         self.sc.nc.z_max = self.maxZSB.value()
         self.sc.nc.n_min = self.minNSB.value()
         self.sc.nc.n_max = self.maxNSB.value()
+        
+        self.sc.nc.n_value = self.valCB.currentIndex()
         
         self.sc.nc.font_size = self.fontSizeSB.value()
         self.sc.nc.border_width = self.borderWidthSB.value()
